c8db8825d0d097fc451b6f3b76b08bed8e925794
Nguyen Trung Duc <ngtrduc1994@gmail.com>
2016-06-28 12:57:12 +0700

    show result

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,19 @@ module ApplicationHelper
     direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
     link_to title, {sort: column, direction: direction}, {class: css_class}
   end
+
+  def answer_icon result
+    true_answer?(result) ? "O" : "X"
+  end
+  
+  def result_class result
+    true_answer?(result) ? "true_answer" : ""
+  end
+
+  private
+  def true_answer? result
+    correct_answers = result.word.word_answers.select {|answer|
+      answer.is_correct?}
+    correct_answers.include? result.word_answer
+  end
 end
